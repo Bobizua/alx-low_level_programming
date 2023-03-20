d22b26f28ef426a49995d9c596023e42e4c345c7
malik <milito4all2@gmail.com>
2023-03-20 08:43:59 -0700

    size

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints the name of file
- * Return: 0;
+ * main - prints the name of the file
+ * Return: 0
  */
 int main(void)
 {
-	printf("%s\n" __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
