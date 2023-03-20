c60bb3f49e93fa3ab5a2e5e1e6c494452fea4f85
malik <milito4all2@gmail.com>
2023-03-20 15:09:58 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_
-#define _DOG_
+#ifndef DOG_H
+#define DOG_H
 int _putchar (char c);
 /**
  * struct dog - data structure
@@ -7,18 +7,18 @@ int _putchar (char c);
  * @age: age of dog
  * @owner: owner of dog
  *
- * Description: dog with name age, and owner's name.
+ * Description: dog with name, age, and owner's name.
  */
 struct dog
 {
-	char  *name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
-typedef struct dog dog_d;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_d *new_dog(char *name, float age, char *owner);
-void free_dog(dog_d *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
